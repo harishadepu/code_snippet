1958f9d7fb4dd3e9c3ba472e0073e1a4dc05e0e1
Harish Adepu <harishadepu40@gmail.com>
2025-11-21 14:45:05 +0530

    Update API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://code-snippet-5t9x.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
